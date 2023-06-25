8f82fb17b84278d3f7179d5f59fe2fa72567d3e8
Weilin Chang <xishanqiming@gmail.com>
2023-06-25 14:44:10 +0800

    Add files via upload

---
+++ Lab3_SortTest_ChangWeilin/HeapSort.xaml.cs
@@ -0,0 +1,259 @@
+﻿/**HeapSort.xaml.cs
+*@author: CHANG WEI-LIN
+*@Last Modify: 4/9/2023
+*@About: This program is the window of the HeapSort Algorithms Test Application With C# WPF.
+*/
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Win32;
+using OxyPlot.Series;
+using OxyPlot;
+using OxyPlot.Wpf;
+using System.Windows.Input;
+using OxyPlot.Axes;
+
+namespace Lab3_SortTest_ChangWeilin
+{
+    //堆排序窗口类
+    public partial class HeapSort : Window
+    {
+        //设置实例字段，_filePath 标记选定文本的绝对路径，_data 数组用于存取数据，_dataSize 用于设置用户选定的数据规模
+        private string _filePath;
+        private int _dataSize;
+        private double[] _data;
+        private double lg_dataSize; 
+        private double ttime;
+
+        //_filePath（选定文件的路径），_dataSize（用户选择的数据大小），_data（用于存储数据的数组），lg_dataSize（数据大小的对数），ttime（总排序时间）
+        //OxyPlot绘图所需的对象：_plotModel（绘图模型），_lineSeries（线条系列），_lastMousePosition（记录上次鼠标位置）
+        private readonly PlotModel _plotModel;
+        private readonly LineSeries _lineSeries;
+        private  ScreenPoint _lastMousePosition;
+
+        //构造函数，初始化各组件，并设置OxyPlot图表的基本属性
+        public HeapSort()
+        {
+            InitializeComponent();
+            cbDataSelection.SelectedIndex = 0;
+
+            _plotModel = new PlotModel
+            {
+                IsLegendVisible = false,
+                PlotAreaBorderColor = OxyColors.Transparent
+            };
+
+            _lineSeries = new LineSeries
+            {
+                MarkerType = MarkerType.Circle,
+                MarkerSize = 5,
+                MarkerStroke = OxyColors.Black
+            };
+
+            _plotModel.Series.Add(_lineSeries);
+            _plotModel.Axes.Add(new LinearAxis { Position = AxisPosition.Bottom, Title = "Data Size (lg)", Maximum = 10 });
+            _plotModel.Axes.Add(new LinearAxis { Position = AxisPosition.Left, Title = "Total Milliseconds" });
+            plotView.Model = _plotModel;
+        }
+
+        //初始化读入文件按钮响应函数，准许读入文本格式的文件，并将文件路径显示在界面上
+        private void ReadFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*"
+            };
+
+            //显示提示信息：读入文本的绝对路径
+            if (openFileDialog.ShowDialog() == true)
+            {
+                _filePath = openFileDialog.FileName;
+                btnReadFile.Content = "文件路径";
+                txtInformation.Text = $"File: {_filePath}";
+            }
+        }
+
+        //初始化选定数据规模按钮响应函数
+        private void DataSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string[] dataOptions = { "10", "100", "1000", "10000", "100000", "1000000", "all" };
+            string selectedDataOption = dataOptions[cbDataSelection.SelectedIndex];
+
+            //若选择全部数据，则不设置最大数据上限（设定为-1）
+            if (selectedDataOption == "all")
+            {
+                _dataSize = -1;
+            }
+            else
+            {
+                _dataSize = int.Parse(selectedDataOption);
+            }
+            //显示提示信息：选择的数据规模
+            txtInformation.Text = $"选定排序的数据规模: {selectedDataOption}";
+        }
+        //初始化开始按钮响应函数
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            //提示错误信息：未选择合法的读入文件
+            if (_filePath == null)
+            {
+                MessageBox.Show("请选择一个读入文件！", "读入文件尚未选择", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            //读入选定文件的数据，以空格' '，作为每个数据的分隔符，加入待排数组中
+            try
+            {
+                string fileContent = File.ReadAllText(_filePath);
+                _data = fileContent.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                                   .Select(double.Parse)
+                                   .ToArray();
+
+                //根据用户设定的数据规模进行限制
+                //只会读入限定规模的数据
+                if (_dataSize != -1 && _data.Length > _dataSize)
+                    _data = _data.Take(_dataSize).ToArray();
+
+                else
+                    _data = _data.ToArray();
+                
+                //数据全部就绪后（也就是读入数据完毕后），开始对构造堆及堆排序的过程计时
+                _data = HeapSortAlgorithm(_data, out TimeSpan heapConstructionTime, out TimeSpan sortingTime);
+                txtInformation.Text = $"构建堆时间为： {heapConstructionTime.TotalMilliseconds} 毫秒; 堆排序时间为： {sortingTime.TotalMilliseconds} 毫秒";
+
+                //将排序完成的数据以文本文件格式输出到原读入文件同目录下，编码格式为 UTF-8，每个数据之间空一个空格（仍为原格式）
+                string outputPath = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(_filePath), "sorted_output.txt");
+                File.WriteAllText(outputPath, string.Join(" ", _data.Select(i => i.ToString())) + " ", Encoding.UTF8);
+                double sortingTimeMs = sortingTime.TotalMilliseconds + heapConstructionTime.TotalMilliseconds;
+
+                MessageBox.Show($"排序成功！总耗时为：{sortingTime.TotalMilliseconds + heapConstructionTime.TotalMilliseconds} 毫秒。排序结果已保存至：{outputPath}", "排序已结束", MessageBoxButton.OK, MessageBoxImage.Information);
+                _filePath = null;
+                ttime = sortingTime.TotalMilliseconds + heapConstructionTime.TotalMilliseconds;
+                lg_dataSize = Math.Log(_data.Length, 10);
+
+                AddPointToLineSeries(_lineSeries, new DataPoint(lg_dataSize, ttime));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"错误: {ex.Message}", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        //向LineSeries添加点，根据x值从小到大排序连成折线
+        private void AddPointToLineSeries(LineSeries lineSeries, DataPoint newPoint)
+        {
+            int index = 0;
+            bool added = false;
+            for (; index < lineSeries.Points.Count; index++)
+            {
+                if (newPoint.X < lineSeries.Points[index].X)
+                {
+                    lineSeries.Points.Insert(index, newPoint);
+                    added = true;
+                    break;
+                }
+            }
+            //如果新添加的点的横坐标是最大值，则将其添加到末尾
+            if (!added)
+                lineSeries.Points.Add(newPoint);
+            _plotModel.InvalidatePlot(true);
+        }
+
+        //堆排序的核心算法，输入一个double类型数组，传入三个参数：数据数组，构造堆的时间，堆排序的时间
+        //输出堆构造时间和堆排序时间和排好序的数据
+        private double[] HeapSortAlgorithm(double[] data, out TimeSpan heapConstructionTime, out TimeSpan sortingTime)
+        {
+            int n = data.Length;
+            Stopwatch stopwatch = new Stopwatch();
+
+            //计算构造堆的时间
+            stopwatch.Start();
+            //构造最大堆
+            for (int i = n / 2 - 1; i >= 0; i--)
+            {
+                LessHeap(data, n, i);
+            }
+            stopwatch.Stop();
+            heapConstructionTime = stopwatch.Elapsed;
+
+            stopwatch.Restart();
+            //遍历堆中每一个元素
+            for (int i = n - 1; i >= 0; i--)
+            {
+                //进行堆排序：交换当前的根节点（即最大值）与最后一个节点
+                (data[i], data[0]) = (data[0], data[i]);
+
+                //递归调用LessHeap函数，实现每次排序后堆的规模都能逐次减少
+                LessHeap(data, i, 0);
+            }
+            stopwatch.Stop();
+            sortingTime = stopwatch.Elapsed;
+            return data;
+        }
+
+        //构建最大堆的函数
+        //对所有受影响的子树进行递归，不断减小堆的规模
+        private void LessHeap(double[] data, int n, int i)
+        {
+            int largest = i;
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+
+            if (left < n && data[left] > data[largest])
+                largest = left;
+
+            if (right < n && data[right] > data[largest])
+                largest = right;
+
+            if (largest != i)
+            {
+                (data[largest], data[i]) = (data[i], data[largest]);
+                LessHeap(data, n, largest);    
+            }
+        }
+
+        private void TxtData_TextChanged(object sender, TextChangedEventArgs e)  
+        { 
+            //啥也没有
+        }
+
+        //界面交互的事件处理函数，用于处理鼠标在OxyPlot图表上的操作
+        private void PlotView_MouseMove(object sender, MouseEventArgs e)
+        {
+            //获取当前鼠标位置并将其转换为OxyPlot的ScreenPoint类型
+            var currentPosition = ToScreenPoint(e.GetPosition(plotView));
+
+            //如果按下鼠标左键
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                //如果_lastMousePosition不是默认值（即有上一个鼠标位置）
+                if (!_lastMousePosition.Equals(default))
+                {
+                    //计算鼠标在X轴和Y轴上的移动距离
+                    var deltaX = currentPosition.X - _lastMousePosition.X;
+                    var deltaY = currentPosition.Y - _lastMousePosition.Y;
+
+                    //根据鼠标在X轴和Y轴上的移动距离移动图表的X轴和Y轴
+                    _plotModel.DefaultXAxis.Pan(-deltaX);
+                    _plotModel.DefaultYAxis.Pan(-deltaY);
+
+                    //更新图表
+                    _plotModel.InvalidatePlot(false);
+                }
+            }
+        }
+
+        //将WPF的Point类型转换为OxyPlot的ScreenPoint类型
+        private static ScreenPoint ToScreenPoint(Point point)
+        {
+            return new ScreenPoint(point.X, point.Y);
+        }
+    }
+}
+
+/* Program Over */
---
+++ Lab3_SortTest_ChangWeilin/MainWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿/**MainWindow.xaml.cs
+*@author: CHANG WEI-LIN
+*@Last Modify: 4/9/2023
+*@About: This program is the main window of this Sort Algorithms Test Application with C# WPF.
+*/
+
+using System.Windows;
+using System.Windows.Input;
+
+namespace Lab3_SortTest_ChangWeilin
+{
+    //Interaction logic for MainWindow.xaml
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        //按钮一：链接到“HeapSort 堆排序”窗体
+        private void Button_HeapClick(object sender, RoutedEventArgs e)
+        {
+            //生成新的主窗体
+            HeapSort HeapSort = new HeapSort
+            {
+                Title = "Heap Sort"
+            };
+
+            //设置系统主窗体
+            App.Current.MainWindow = HeapSort;
+            HeapSort.Owner = this;
+
+            // 显示新窗口为模式对话框
+            _ = HeapSort.ShowDialog();
+        }
+
+        //按钮二：链接到“QuickSort 快速排序”窗体
+        private void Button_QuickClick(object sender, RoutedEventArgs e)
+        {
+            //生成新的主窗体
+            QuickSort QuickSort = new QuickSort
+            {
+                Title = "Quick Sort"
+            };
+
+            //设置系统主窗体
+            App.Current.MainWindow = QuickSort;
+            QuickSort.Owner = this;
+
+            // 显示新窗口为模式对话框
+            _ = QuickSort.ShowDialog();
+        }
+
+        //按钮三：链接到“Bubble Sort”窗体
+        private void Button_BubbleClick(object sender, RoutedEventArgs e)
+        {
+            //生成新的主窗体
+            BubbleSort BubbleSort = new BubbleSort
+            {
+                Title = "Bubble Sort"
+            };
+
+            //设置系统主窗体
+            App.Current.MainWindow = BubbleSort;
+            BubbleSort.Owner = this;
+
+            // 显示新窗口为模式对话框
+            _ = BubbleSort.ShowDialog();
+        }
+
+        //用户点击按钮一 Heap Sort 时消息栏的反馈
+        private void Button_HeapClick_MouseEnter(object sender, MouseEventArgs e)
+        {
+            StatusBarText.Text = " Heap Sort 堆排序，通过构建一个最大堆，进行排序。 ";
+        }
+
+        //用户光标移开后消息栏的反馈
+        private void Button_HeapClick_MouseLeave(object sender, MouseEventArgs e)
+        {
+            StatusBarText.Text = string.Empty;
+        }
+
+        //用户点击按钮二 Quick Sort 时消息栏的反馈
+        private void Button_QuickClick_MouseEnter(object sender, MouseEventArgs e)
+        {
+            StatusBarText.Text = " Quick Sort 快速排序，选定基准，将序列分为两部分。 ";
+        }
+
+        //用户光标移开后消息栏的反馈
+        private void Button_QuickClick_MouseLeave(object sender, MouseEventArgs e)
+        {
+            StatusBarText.Text = string.Empty;
+        }
+
+        //用户点击按钮三 Bubble Sort 时消息栏的反馈
+        private void Button_BubbleClick_MouseEnter(object sender, MouseEventArgs e)
+        {
+            StatusBarText.Text = " Bubble Sort 冒泡排序，每趟排序都返回当前序列的最小值。 ";
+        }
+
+        //用户光标移开后消息栏的反馈
+        private void Button_BubbleClick_MouseLeave(object sender, MouseEventArgs e)
+        {
+            StatusBarText.Text = string.Empty;
+        }
+
+        //用户点击作者信息时消息栏的反馈
+        private void PerPho_MouseEnter(object sender, MouseEventArgs e)
+        {
+            StatusBarText.Text = " 绿色与自由软件倡导者同盟，CHANG WEI-LIN 制作。";
+        }
+
+        //用户光标移开后消息栏的反馈
+        private void PerPho_MouseLeave(object sender, MouseEventArgs e)
+        {
+            StatusBarText.Text = string.Empty;
+        }
+    }
+}
+
+/* Program Over */
